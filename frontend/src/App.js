27aeb2b83c74b2e5c7aff849fc29048c9fe0f5d0
mzsellem <marleysellem@gmail.com>
2023-10-06 14:35:49 -0400

    test front-back-end connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { details: [] }
+
+  componentDidMount() {
+    let data;
+    axios.get('http://localhost:8000')
+      .then(res => {
+        data = res.data;
+        this.setState({
+          details: data
+        });
+      })
+      .catch(err => { })
+  }
+  render() {
+    return(
+      <div>
+        <header>Data Generated From Django</header>
+        <hr></hr>
+        {this.state.details.map((output, id)=> (
+          <div key={id}>
+            <div>
+              <h2>{output.employee}</h2>
+              <h3>{output.department}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
