cb389daf981ed466ef12f8db3cf232991ed8c405
mzsellem <marleysellem@gmail.com>
2023-10-08 13:12:00 -0400

    add react pdf viewer and resume

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,43 +1,27 @@
-// import Navbar from "../components/navbar";
-// import { Worker } from "@react-pdf-viewer/core";
-// import { Viewer } from "@react-pdf-viewer/core";
-// import "@react-pdf-viewer/core/lib/styles/index.css";
-// import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
-// import "@react-pdf-viewer/page-navigation/lib/styles/index.css";
+import Navbar from "../components/navbar";
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+import { pageNavigationPlugin } from "@react-pdf-viewer/page-navigation";
+import "@react-pdf-viewer/page-navigation/lib/styles/index.css";
 
-// import * as React from "react";
-import React, { useState, useEffect } from "react";
+import * as React from "react";
 // import Pagination from "@mui/material/Pagination";
 // import Stack from "@mui/material/Stack";
 
 export default function Home() {
-   //    const pageNavigationPluginInstance = pageNavigationPlugin();
-
-   //    const { CurrentPageInput, GoToNextPageButton, GoToPreviousPage } =
-   //       pageNavigationPluginInstance;
+   const pageNavigationPluginInstance = pageNavigationPlugin();
 
    return (
       <>
-         {/* <Navbar /> */}
-         {/* <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+         <Navbar />
+         <div className="flex text-3xl justify-center p-8">Hello BeyondMD!</div>
+         <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
             <Viewer
                fileUrl="resume.pdf"
                plugins={[pageNavigationPluginInstance]}
             />
          </Worker>
-         <div className="flex flex-row w-full justify-between p-6">
-            <div className="flex flex-col border w-1/2">
-               <div className="flex justify-center text-3xl"></div>
-               <div className="flex justify-center">
-                  <Stack spacing={2}>
-                     <Pagination count={2} color="primary" />
-                  </Stack>
-               </div>
-            </div> */}
-         <div className="flex text-3xl border justify-center rounded">
-            Hello BeyondMD!
-         </div>
-         {/* </div> */}
       </>
    );
 }
