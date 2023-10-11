cc962e64842fc8bc8a9fcaf96fc1cb3ee55f6629
mzsellem <marleysellem@gmail.com>
2023-10-11 10:12:05 -0400

    remove unused elements and comment code in navbar.jsx

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,8 +1,3 @@
-{
-   /* <Link to="/patients">Patients</Link>
- <Link to="/">Home</Link> */
-}
-
 import * as React from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
@@ -10,38 +5,16 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-// import AdbIcon from "@mui/icons-material/Adb";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 
 const pages = ["Home", "Patients"];
-// const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
+//App bar from MUI
 export default function ResponsiveAppBar() {
-   const [anchorElNav, setAnchorElNav] = React.useState(null);
-   const [anchorElUser, setAnchorElUser] = React.useState(null);
-
-   const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-   };
-   const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-   };
-
-   const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-   };
-
-   const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-   };
-
    return (
       <AppBar position="static">
          <Container maxWidth="xl">
@@ -66,52 +39,15 @@ export default function ResponsiveAppBar() {
                >
                   BeyondMD
                </Typography>
-
-               <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                  <IconButton
-                     size="large"
-                     aria-label="account of current user"
-                     aria-controls="menu-appbar"
-                     aria-haspopup="true"
-                     onClick={handleOpenNavMenu}
-                     color="inherit"
-                  >
-                     <MenuIcon />
-                  </IconButton>
-                  <Menu
-                     id="menu-appbar"
-                     anchorEl={anchorElNav}
-                     anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "left",
-                     }}
-                     keepMounted
-                     transformOrigin={{
-                        vertical: "top",
-                        horizontal: "left",
-                     }}
-                     open={Boolean(anchorElNav)}
-                     onClose={handleCloseNavMenu}
-                     sx={{
-                        display: { xs: "block", md: "none" },
-                     }}
-                  >
-                     {pages.map((page) => (
-                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                           <Typography textAlign="center">{page}</Typography>
-                        </MenuItem>
-                     ))}
-                  </Menu>
-               </Box>
                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                   {pages.map((page) => (
                      <Link
                         key={page}
+                        //Clicked on the Home link? Go to "/", if not, then go to "/name of page clicked on"
                         to={page === "Home" ? "/" : `/${page.toLowerCase()}`}
                         style={{ textDecoration: "none", color: "inherit" }}
                      >
                         <Button
-                           onClick={handleCloseNavMenu}
                            sx={{ my: 2, color: "white", display: "block" }}
                         >
                            {page}
@@ -122,7 +58,7 @@ export default function ResponsiveAppBar() {
 
                <Box sx={{ flexGrow: 0 }}>
                   <Tooltip>
-                     <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                     <IconButton sx={{ p: 0 }}>
                         <Avatar alt="Marley's Avatar" src="/flower.jpg" />
                      </IconButton>
                   </Tooltip>
